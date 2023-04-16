f5ecbbb72d4fa0a348ab7483f2200983a3c9ffa0
Ronak Manglani <ronak.manglani@gmail.com>
2023-04-16 15:17:07 +0530

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,27 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 import * as packageJson from "./package.json";
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [
+    basicSsl(),
     react(),
     tsConfigPaths(),
     dts({
-      include: ["src/Camera/"],
+      include: ["src/lib/"],
     }),
   ],
   build: {
     minify: true,
     lib: {
-      entry: resolve("src", "Camera/index.ts"),
+      entry: resolve("src", "lib/index.ts"),
       name: "ReactUseCamera",
       formats: ["es", "umd"],
       fileName: (format) => `react-use-camera.${format}.js`,
