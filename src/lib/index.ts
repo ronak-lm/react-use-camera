5475803288bd2c0468bd345bd4bd123112b2948f
Ronak Manglani <ronak.manglani@gmail.com>
2024-07-07 13:08:49 +0530

    export types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,11 @@ export { useCamera } from "./useCamera";
 export { default as Camera } from "./Camera";
 export type { CameraProps } from "./Camera";
 
-export type { CameraHandle, CameraElement, CaptureSettings, CapturedImage } from "./types";
+export type {
+  CameraHandle,
+  CameraElement,
+  CaptureSettings,
+  VideoCaptureParams,
+  CapturedImage,
+  CapturedVideo,
+} from "./types";
